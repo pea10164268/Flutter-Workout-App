e3eb658db865882fcca28aeb65058fa3079284c7
pea10164268 <73113042+pea10164268@users.noreply.github.com>
2022-03-15 11:03:12 +0000

    Create activity page created and styled

---
+++ lib/screens/create/create_activity.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class CreateActivity extends StatefulWidget {
+  const CreateActivity({Key? key}) : super(key: key);
+
+  @override
+  State<CreateActivity> createState() => _CreateActivityState();
+}
+
+class _CreateActivityState extends State<CreateActivity> {
+  final TextEditingController name = TextEditingController();
+  final TextEditingController description = TextEditingController();
+  final TextEditingController type = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text("Create your activity",
+              style: TextStyle(color: Colors.white)),
+          backgroundColor: Colors.blueGrey.shade300,
+          centerTitle: true,
+          elevation: 0,
+      ),
+      body: Container(
+        width: (MediaQuery.of(context).size.width),
+        height: MediaQuery.of(context).size.height,
+        color: Colors.blueGrey.shade50,
+        padding: const EdgeInsets.all(32),
+        child: SingleChildScrollView(
+          child: Form(
+            child: Column(
+              children: <Widget> [
+                TextFormField(
+                  controller: name,
+                  decoration: const InputDecoration(
+                    labelText: 'Name'
+                  ),
+                ),
+                TextFormField(
+                  controller: description,
+                  decoration: const InputDecoration(
+                      labelText: 'Description'
+                  ),
+                ),
+                TextFormField(
+                  controller: type,
+                  decoration: const InputDecoration(
+                      labelText: 'Type'
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Create',
+                    style: TextStyle( fontSize: 20),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor:
+                    MaterialStateProperty.all<Color>(Colors.black),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      )
+    );
+  }
+}
--- lib/screens/main.dart
+++ lib/screens/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_workout_app/screens/register.dart';
+import 'package:flutter_workout_app/screens/create/create_activity.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Login(),
+      home: CreateActivity(),
       debugShowCheckedModeBanner: false,
     );
   }
